50eb6314c8069b61f9f97ca70d6c41fdc2197898
Sitri2002 <nguyenduyanh241002@gmail.com>
2024-11-20 09:17:20 -0700

    App template

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-fn main() {
-    println!("Hello, world!");
-    println!("Test");
+use eframe::egui;
+
+fn main() -> eframe::Result {
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("Calorie Calculator", native_options, Box::new(|cc| Ok(Box::new(MyEguiApp::new(cc)))))
 }
 
+#[derive(Default)]
+struct MyEguiApp {}
+
+impl MyEguiApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        Self::default()
+    }
+}
+
+impl eframe::App for MyEguiApp {
+   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Test");
+       });
+   }
+}
